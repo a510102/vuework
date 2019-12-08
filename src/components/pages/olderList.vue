9ebb6944a52e5bcc9547f33c2ea53e95701ac2c3
Jay Hung <b98310047@hotmail.com>
2019-12-08 16:50:43 +0800

    add file 2

--- src/components/pages/olderList.vue
+++ src/components/pages/olderList.vue
@@ -9,37 +9,58 @@
         <th width="120">是否付款</th>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>2</td>
-          <td class="text-right">
-            3
-          </td>
-          <td class="text-right">
-            4
-          </td>
+        <tr v-for="(item, key) in orders" :key="key"
+          :class="{'text-secondary': !item.is_paid}">
+          <td>{{ item.create_at | date }}</td>
+          <td><span v-text="item.user.email" v-if="item.user"></span></td>
           <td>
-            5
+            <ul class="list-unstyled">
+              <li v-for="(product, i) in item.products" :key="i">
+                {{ product.product.title }} 數量：{{ product.qty }}
+                {{ product.product.unit }}
+              </li>
+            </ul>
+          </td>
+          <td class="text-right">{{ item.total | currency }}</td>
+          <td>
+            <strong v-if="item.is_paid" class="text-success">已付款</strong>
+            <span v-else class="text-muted">尚未起用</span>
           </td>
         </tr>
       </tbody>
     </table>
+    <Pagination :pagination= "pagination"
+                v-on:changepages="getProducts"/>
   </div>
 </template>
 
 <script>
+import Pagination from './Pagination.vue';
+
 export default {
+  data() {
+    return {
+      orders: {},
+      isNew: false,
+      pagination: {},
+      isLoading: false,
+    };
+  },
+  components: { Pagination },
   methods: {
-    getProducts() {
-      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_CUSTOMPATH}/admin/orders`;
-      // const vm = this;
+    getProducts(page = 1) {
+      const vm = this;
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_CUSTOMPATH}/orders?page=${page}`;
       this.$http.get(api).then((response) => {
-        console.log(response);
+        vm.orders = response.data.orders;
+        vm.isLoading = false;
+        vm.pagination = response.pagination;
+        console.log(vm.pagination);
       });
     },
-    created() {
-      this.getProducts();
-    },
+  },
+  created() {
+    this.getProducts();
   },
 };
 </script>
